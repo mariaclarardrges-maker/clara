46dfef9960f10089781ac9bede1249fb0a0a1416
Koditinho <266051778+Koditinho@users.noreply.github.com>
2026-04-10 16:13:06 -0300

    fh

---
+++ Assets/Movement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float velocity = 1f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        this.transform.position += new Vector3(horizontalInput*velocity,0,0);
+        this.transform.position += new Vector3(0,verticalInput*velocity,0);
+        if(this.transform.position.x>10)
+        {
+            this.transform.position = new Vector3 (10, this.transform.position.y, this.transform.position.z);
+        } else if (this.transform.position.x<-10) {
+            this.transform.position = new Vector3 (-10, this.transform.position.y, this.transform.position.z);
+        }
+    }
+}
