5fc63db6743abc3510137b9ac6e4697e1dc99e08
mariaclarardrges-maker <maria.clarardrges@gmail.com>
2026-05-08 15:31:59 -0300

    08/05 2.0

--- Assets/PegarLixo.cs
+++ Assets/PegarLixo.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PegarLixo : MonoBehaviour
-{
+{   
+    public AudioSource source; 
     public LixoSpawnerController lixoSpawnerController;
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Lixo"))
         {
+            source.Play();
             Destroy(collision.gameObject);
             lixoSpawnerController.AddToPoints(1);
         }
